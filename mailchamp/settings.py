ddc44d9ca5bbc3205b23845004b0af6c1f967fd6
Tushar <tusharverma643@gmail.com>
2022-05-07 23:14:31 +0530

    updated requirements.txt

--- mailchamp/settings.py
+++ mailchamp/settings.py
@@ -127,7 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
